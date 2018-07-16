b52e3a5afe319ed5150a0774d6b456db3f07d309
yb <yb@ybs-MacBook-Pro.local>
2018-07-15 21:40:17 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bulma/css/bulma.css'
+
+
+class App extends Component {
+
+
+
+//  this.state.response.map((items) => items.data.preview.images.map((memeUrl)=> <li>{memeUrl.source.url}</li>))
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      response: [],
+    };
+    this.memesList = this.memesList.bind(this);
+
+  }
+
+  memesList() {
+      var listItems = this.state.response.map((items) => items.data.preview.images.map((memeUrl)=>
+      // <Col xs={6} md={4}>
+      //   <Image src={memeUrl.source.url} responsive />
+      // </Col>
+
+      <div class="column is-one-third">
+                    <div class="card large">
+                        <div class="card-image">
+                            <figure class="image">
+                                <img src={memeUrl.source.url} alt="Image"/>
+                            </figure>
+                        </div>
+                      </div>
+                </div>
+
+    ))
+      return (
+        <ul>{listItems}</ul>
+      );
+    }
+
+
+  componentDidMount() {
+    fetch("https://www.reddit.com/r/meme/new/.json")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          response: result.data.children
+        });
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+  }
+
+  render() {
+
+    var response;
+    var memelist
+    console.log(this.state.response.map((items) => items.data.preview.images.map((memeUrl)=> memeUrl.source.url)))
+
+    if(this.state.response){
+      response = this.state.response
+    }
+
+
+
+
+    return (
+<div className="App">
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1 className="App-title">YB Memes</h1>
+  </header>
+
+<div class="row columns">
+<section class="hero is-info is-medium is-bold">
+  {this.memesList()}
+</section>
+  </div>
+</div>
+    );
+  }
+}
+
+export default App;
