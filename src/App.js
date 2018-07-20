c46e1297850cf739bb6425e71fef7ec129edafd7
yb <yb@ybs-MacBook-Pro.local>
2018-07-19 23:33:48 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'bulma/css/bulma.css'
-
+import 'bulma/css/bulma.css';
+import axios from 'axios'
 
 class App extends Component {
-
-
-
-//  this.state.response.map((items) => items.data.preview.images.map((memeUrl)=> <li>{memeUrl.source.url}</li>))
 
   constructor(props) {
     super(props);
@@ -16,77 +12,73 @@ class App extends Component {
       error: null,
       isLoaded: false,
       response: [],
+      loaded: false,
+      urlArr: []
     };
-    this.memesList = this.memesList.bind(this);
-
+    this.getSubreddit = this.getSubreddit.bind(this);
+    this.updateMemeUrl = this.updateMemeUrl.bind(this);
   }
 
-  memesList() {
-      var listItems = this.state.response.map((items) => items.data.preview.images.map((memeUrl)=>
-      <div class="column is-one-third">
-                    <div class="card large">
+  componentDidMount() {
+    this.getSubreddit()
+  }
+
+
+
+  getSubreddit() {
+    var memeDivs = axios.get('https://www.reddit.com/r/meme/new/.json').then(apiResponse => this.setState({response: apiResponse.data.data.children, loaded: true}))
+  }
+
+  updateMemeUrl() {
+    const stateResp = this.state.response
+    var arr = []
+    const divArr = []
+
+
+    console.log(this.state.loaded)
+    if (this.state.loaded===true) {
+        for (var i = 0; i < stateResp.length; i++) {
+          arr.push(stateResp[i].data.url)
+        }
+        console.log(arr)
+        //add to div array
+        for (var j = 0; j < arr.length; j++) {
+          divArr.push(
+            <div class="column is-one-third">
+                    <div class="card small">
                         <div class="card-image">
                             <figure class="image">
-                                <img src={memeUrl.source.url} alt="Image"/>
+                                <img src={arr[j]} alt="Image" />
                             </figure>
                         </div>
                       </div>
                 </div>
-    ))
-      return (
-        <div class="columns is-multiline wrapper">{listItems}</div>
-      );
-    }
-
-
-  componentDidMount() {
-    fetch("https://www.reddit.com/r/meme/new/.json")
-    .then(res => res.json())
-    .then(
-      (result) => {
-        this.setState({
-          isLoaded: true,
-          response: result.data.children
-        });
-      },
-      // Note: it's important to handle errors here
-      // instead of a catch() block so that we don't swallow
-      // exceptions from actual bugs in components.
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
+              )
+          console.log(arr[j])
+        }
       }
-    )
+
+
+      return <div class="row columns is-multiline wrapper">{divArr}</div>
   }
 
-  render() {
+  render(
+  ){
+            return (<div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1 className="App-title">
+                  MommaMemes
+                </h1>
+                <div>Freshest Memes daily... Just How Momma Use to Make!</div>
+              </header>
+              <div>{this.updateMemeUrl()}</div>
+            </div>);
 
-    var response;
-    var memelist
-    console.log(this.state.response.map((items) => items.data.preview.images.map((memeUrl)=> memeUrl.source.url)))
-
-    if(this.state.response){
-      response = this.state.response
-    }
-
-
-
-
-    return (
-<div className="App">
-  <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <h1 className="App-title">
-    MommaMemes
-    </h1>
-    <div>Freshest Memes daily... Just How Momma Use to Make!</div>
-  </header>
-  {this.memesList()}
-</div>
-    );
   }
+
+
 }
+
 
 export default App;
